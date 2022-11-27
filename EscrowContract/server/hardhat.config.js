d1300d1819587a1cae62360d9bd39adfff4ddbea
archana-sing <archanasingh1643@gmail.com>
2022-11-27 09:24:38 -0800

    integrated frontend for escrow contract

--- EscrowContract/server/hardhat.config.js
+++ EscrowContract/server/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/4-dFvfsLXlMzunhf8snXiwsDed8Wxf1o`,
-      accounts: ["746b608b6030733cfed644842d9b457fee38e2f008854597547baf79c2845fa1"]
+      accounts: []
     }
   }
 };
